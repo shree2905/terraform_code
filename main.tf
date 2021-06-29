d346782066540a6e6d45d6b55d9ee070b8b7c85a
shree2905 <shridhardeshpande1997@gmail.com>
2021-06-29 16:48:18 +0530

    removed service principle

--- main.tf
+++ main.tf
@@ -9,10 +9,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "bcb90759-77e9-40de-824e-4598ecc74a22"
-  client_id       = "f8a9371a-9641-4b51-9471-ae44c0268d6a"
-  client_secret   = "ldBHE2MLgCaChx4o08ndC-KE9nA2c_~V_Z"
-  tenant_id       = "6dd0e481-0ee7-48c1-9a0a-bff931424a20"
 }
 
 #create resource group
